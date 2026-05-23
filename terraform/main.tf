184eeff671905c3e3a5b3a2ad987e923f9c372d4
Pragati2708 <pragatisingh2708@gmail.com>
2026-05-23 22:11:45 +0530

    #1

--- terraform/main.tf
+++ terraform/main.tf
@@ -299,3 +299,86 @@ resource "aws_lb_target_group" "tutus_pizza_tg_green" {
     Name = "Tutus Pizza Green Target Group"
   }
 }
+resource "aws_codedeploy_app" "tutus_pizza_app" {
+  compute_platform = "ECS"
+
+  name = "tutus-pizza-codedeploy-app"
+}
+resource "aws_iam_role" "codedeploy_role" {
+  name = "tutus-pizza-codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
+  role = aws_iam_role.codedeploy_role.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
+}
+resource "aws_codedeploy_deployment_group" "tutus_pizza_dg" {
+  app_name               = aws_codedeploy_app.tutus_pizza_app.name
+
+  deployment_group_name  = "tutus-pizza-deployment-group"
+
+  service_role_arn       = aws_iam_role.codedeploy_role.arn
+
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+
+  deployment_style {
+    deployment_type   = "BLUE_GREEN"
+
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+  }
+
+  blue_green_deployment_config {
+
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.tutus_pizza_cluster.name
+
+    service_name = aws_ecs_service.tutus_pizza_service.name
+  }
+
+  load_balancer_info {
+
+    target_group_pair_info {
+
+      prod_traffic_route {
+        listener_arns = [
+          aws_lb_listener.http_listener.arn
+        ]
+      }
+
+      target_group {
+        name = aws_lb_target_group.tutus_pizza_tg.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.tutus_pizza_tg_green.name
+      }
+    }
+  }
+}
